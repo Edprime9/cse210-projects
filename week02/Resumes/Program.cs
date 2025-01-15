82b031823458e0686014c527e5074a8f01b3dd61
Edprimw9 <eli24007@byui.edu>
2025-01-15 20:19:43 +0100

    finished the project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create the first job instance
+        Job job1 = new Job
+        {
+            _jobTitle = "Project Manager",
+            _company = "PrimeHub Solution",
+            _startYear = 2024,
+            _endYear = 2025
+        };
+
+        // Create the second job instance
+        Job job2 = new Job
+        {
+            _jobTitle = "Social Media Manager",
+            _company = "Pixel Palace",
+            _startYear = 2022,
+            _endYear = 2025
+        };
+
+        // Create the resume instance
+        Resume myResume = new Resume
+        {
+            _name = "Eli Ibitamuno"
+        };
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
