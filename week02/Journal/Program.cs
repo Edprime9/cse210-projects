53d387fda883baa70dce545d78e9d5883beca154
Edprimw9 <eli24007@byui.edu>
2025-01-18 22:28:47 +0100

    working top level loop

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        while (true)
+        {
+        Console.WriteLine("1. Write new entry");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+
+        Console.Write("Enter selecton: ");
+        string userInput = Console.ReadLine();
+
+        Console.WriteLine($"User Selected {userInput}");
+        }
     }
 }
