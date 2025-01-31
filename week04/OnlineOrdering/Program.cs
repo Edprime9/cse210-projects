bc58a327823bad803a4e1deb351feb6c32470a5a
Edprimw9 <eli24007@byui.edu>
2025-01-31 16:01:39 +0100

    Done with Project

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,8 +1,45 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+class Order
 {
-    static void Main(string[] args)
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
     {
-        Console.WriteLine(Hello the online ordering);
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public decimal GetTotalCost()
+    {
+        decimal total = 0;
+        foreach (var product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        decimal shippingCost = _customer.IsInUSA() ? 5 : 35;
+        return total + shippingCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in _products)
+        {
+            label += $"- {product.GetPackingLabel()}\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
 }
