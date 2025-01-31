0f659ebeaa4fbe6bdc0e6932b3dea0021eacc8fd
Edprimw9 <eli24007@byui.edu>
2025-01-31 15:12:25 +0100

    Finish YoutudeVideoProject

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,19 +1,40 @@
 using System;
 using System.Collections.Generic;
-
-class Comment
+class Video
 {
-    public string Name { get; }
-    public string Text { get; }
+    public string Title { get; }
+    public string Author { get; }
+    public int Length { get; }
+    private List<Comment> _comments = new List<Comment>();
 
-    public Comment(string name, string text)
+    public Video(string title, string author, int length)
     {
-        Name = name;
-        Text = text;
+        Title = title;
+        Author = author;
+        Length = length;
     }
 
-    public void Display()
+    public void AddComment(Comment comment)
     {
-        Console.WriteLine($" - {Name}: {Text}");
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in _comments)
+        {
+            comment.Display();
+        }
+        Console.WriteLine("----------------------------------");
     }
 }
