3fcfe9b1cd5fc5203526e21945c31d7d993cc988
Edprimw9 <eli24007@byui.edu>
2025-02-07 12:04:29 +0100

    Created a Contructor, prompted the user to breathe in and out for 15sec and created a countdown time,

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+class BreathingActivity : Activity
 {
-    static void Main(string[] args)
+    
+    /* BreathingActivity() is a special method 
+    called a constructor that is used to
+     initialize objects */
+    public BreathingActivity() : base("Breathing Activity", "Focus on your breathing to relax.", 15) {}
+    
+    public override void Run()
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+       DisplayStartingMessage(); //Displays a starting message
+        for (int i = 0; i < _duration / 3; i++)
+        {
+            Console.WriteLine("Breathe in..."); //Prompts the user to inhale.
+            ShowCountDown(3); //Displays Counts down
+            Console.WriteLine("Breathe out..."); // Prompts the user to exhale.
+            ShowCountDown(3);
+    }
+    DisplayEndingMessage();
     }
 }
