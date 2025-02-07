92d2c018bf0759fd373118e310f9db8bc69ff0e3
Edprimw9 <eli24007@byui.edu>
2025-02-08 00:28:57 +0100

    created a list of reflective thoughts and follow up questions and generated a radom number.

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+class ReflectingActivity : Activity
 {
-    static void Main(string[] args)
+    private List<string> _prompts = new List<string> { "Think about a time you overcame a challenge.", "Think about a moment of kindness you experienced." };
+    private List<string> _questions = new List<string> { "How did you feel?", "What did you learn?", "How has this affected you?" };
+
+    public ReflectingActivity() : base("Reflecting Activity", "Reflect on past experiences with guiding questions.", 10) {}
+
+    public override void Run()
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        DisplayStartingMessage();
+        Console.WriteLine(GetRandomPrompt());
+        ShowSpinner(3);
+        DisplayQuestions();
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        return _prompts[rand.Next(_prompts.Count)];
+    }
+
+    private void DisplayQuestions()
+    {
+        foreach (var question in _questions)
+        {
+            Console.WriteLine(question);
+            ShowSpinner(3);
+        }
     }
 }
