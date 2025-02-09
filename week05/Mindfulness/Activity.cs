f8ac631ccb3dc7fb423a1dc04871a37644847cf0
Edprimw9 <eli24007@byui.edu>
2025-02-09 19:12:59 +0100

    added codes to the activity class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+abstract class Activity
 {
-    static void Main(string[] args)
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description, int duration)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        _name = name;
+        _description = description;
+        _duration = duration;
     }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Starting {_name}: {_description}");
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine($"Ending {_name}. Well done!");
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    protected void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write($"{i} ");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    public abstract void Run();
 }
