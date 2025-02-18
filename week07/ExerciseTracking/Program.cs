3e1300c1629741d7f1e112c9cd13f39d7e46d6f1
Edprimw9 <eli24007@byui.edu>
2025-02-18 13:24:16 +0100

    xx

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -6,9 +6,9 @@ class Program
         List<Activity> activities = new List<Activity>();
 
         // Add activities
-        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0)); // Running for 30 minutes with 3 miles
-        activities.Add(new Cycling(new DateTime(2022, 11, 3), 30, 12.0)); // Cycling for 30 minutes at 12 mph
-        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 40)); // Swimming for 30 minutes with 40 laps
+        activities.Add(new Running(new DateTime(2025, 01, 30), 30, 3.0)); // Running for 30 minutes with 3 miles
+        activities.Add(new Cycling(new DateTime(2025, 01, 30), 30, 12.0)); // Cycling for 30 minutes at 12 mph
+        activities.Add(new Swimming(new DateTime(2025, 01, 30), 30, 40)); // Swimming for 30 minutes with 40 laps
 
         // Iterate through the activities and print summaries
         foreach (var activity in activities)
